fc062f27ed7908132645d3d6daba8c0726544e06
HangX-Ma <m-contour@qq.com>
2023-11-02 16:19:01 +0800

    feat: add platform recognition

--- demo/kf_vs_const_model.cc
+++ demo/kf_vs_const_model.cc
@@ -9,8 +9,17 @@
 
 namespace plt = matplotlibcpp;
 
-constexpr const uint32_t kScreenWidth = 1920;
-constexpr const uint32_t kScreenHeight = 1080;
+constexpr const uint32_t kScreenWidth = 2400;
+constexpr const uint32_t kScreenHeight = 1344;
+/**
+ * Camera parameters
+ *  - 16:9 S2
+ * 	- CMOS size 36.0x24.0 mm
+ * 
+ * @ref https://cam.start.canon/zh/C013/manual/html/UG-10_Reference_0100.html
+ */
+constexpr const double kPlaneWidth = 0.036;	 // 36 mm
+constexpr const double kPlaneHeight = 0.024; // 24 mm
 
 int main (int argc,char *argv[]) {
     (void)argc;
